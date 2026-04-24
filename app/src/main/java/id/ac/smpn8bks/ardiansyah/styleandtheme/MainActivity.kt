f3aaa8567c7302ef30587056361a58e816355895
SMC <@ardhycrjr24>
2026-04-24 10:16:28 +0700

    Step1

---
+++ app/src/main/java/id/ac/smpn8bks/ardiansyah/styleandtheme/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.smpn8bks.ardiansyah.styleandtheme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
